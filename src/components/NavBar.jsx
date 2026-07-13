7d70b309a026b484dee73e0701447a699b5cc058
nikhileashy <nikhileashy9@gmail.com>
2026-07-13 10:22:33 +0530

    New Design

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,39 +3,54 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <Link class="navbar-brand" to="/">
+    <nav className="navbar navbar-expand-lg" style={{ 
+      backgroundColor: "#0077ff", 
+      color: "#fff",
+      textColor: "#fff",
+      boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
+      padding: "12px 20px"
+    }}>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/" style={{ color: "#ffffff", fontWeight: "600" }}>
           Blood Bank App
         </Link>
+        
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
+          style={{ border: "1px solid #edf2f7" }}
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" to="/add">
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/" style={{ color: "#fff" }}>
+                Home
+              </Link>
+            </li>
+            
+            <li className="nav-item">
+              <Link className="nav-link" to="/add" style={{ color: "#fff" }}>
                 Add Donor Form
               </Link>
             </li>
 
-            <li class="nav-item">
-              <Link class="nav-link" aria-disabled="true" to="/view">
+            <li className="nav-item">
+              <Link className="nav-link" to="/view" style={{ color: "#fff" }}>
                 View All Donors
               </Link>
             </li>
           </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
